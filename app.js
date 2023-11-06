788e287dc0fe9008349eec9bc48efe8a26daa3be
imRkofficial <rkbagra0490@gmail.com>
2023-11-06 17:05:11 +0530

    initial commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({
 app.use(cookieParser())
 app.use(cors({
     origin:process.env.FRONTEND_URL,
-    credentials:false,
+    credentials:true,
     methods:['GET','POST','PUT','DELETE']
 }))
 
