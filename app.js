f69ab85bdb81720644d15b4223d4474fc2ecb52a
imRkofficial <rkbagra0490@gmail.com>
2023-12-02 11:44:01 +0530

    initial commit

--- app.js
+++ app.js
@@ -31,11 +31,11 @@ app.use(cors({
 app.use(express.urlencoded({
     extended: true
 }));
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", ['http://localhost:3000/login']); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", ['http://localhost:3000/login']); // update to match the domain you will make the request from
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // Importing and Using routes
 import course from './routes/CourseRoutes.js';
