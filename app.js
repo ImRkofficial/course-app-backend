48f46be0d1a215b419370911285c693dd83b3699
imRkofficial <rkbagra0490@gmail.com>
2023-12-02 10:54:15 +0530

    initial commit

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000/"|| "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000/login"|| "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
