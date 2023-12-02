20cd733ecda7203522ea4283cf0c2c3d16e4e2bf
imRkofficial <rkbagra0490@gmail.com>
2023-12-02 11:41:32 +0530

    initial commit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser())
 
 app.use(cors({
     origin:['http://localhost:3000/login'],
-    credentials: true,
+    withCredentials: true,
     methods:["GET","POST","PUT","DELETE"]
 }));
 app.use(express.urlencoded({
