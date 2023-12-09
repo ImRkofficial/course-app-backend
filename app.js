385a8e09447e4bb271190b40e3ba676d493b952e
imRkofficial <rkbagra0490@gmail.com>
2023-12-09 15:40:01 +0530

    Initial commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", 'http://localhost:3000/login'); // update to match the domain you will make the request from
+    res.setHeader("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
